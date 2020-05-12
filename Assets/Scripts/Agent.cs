d69e4ce3f00318bcc65ff2d015a47fbdf918c35f
Daniel Nakhimovich <dnahimov@gmail.com>
2020-05-12 19:47:30 -0400

    pointing in the right direction

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,36 +1,32 @@
 ﻿// using System.Collections;
-
 // using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
-    public float radius;
-    public float mass;
-    public float perceptionRadius;
+    public float thrustForce = 2000f;
+    // public float liftForce = 8f;
+    // public float dragForce = 0.2f;
+    public float maxTorque = 500000;
+
+    public float perceptionDistance;
+    public float perceptionAngle;
 
     private List<Vector3> path;
-    private NavMeshAgent nma;
     private Rigidbody rb;
 
     private HashSet<GameObject> perceivedNeighbors = new HashSet<GameObject>();
-    private HashSet<GameObject> perceivedWalls = new HashSet<GameObject>();
-
-    private bool isCrowded = false;
+    private HashSet<GameObject> perceivedObstacles = new HashSet<GameObject>();
 
     void Start()
     {
         path = new List<Vector3>();
-        nma = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
 
-        gameObject.transform.localScale = new Vector3(2 * radius, 1, 2 * radius);
-        nma.radius = radius;
-        rb.mass = mass;
-        GetComponent<SphereCollider>().radius = perceptionRadius / 2;
+        // gameObject.transform.localScale = new Vector3(2 * radius, 1, 2 * radius);
+        // GetComponent<CapsuleCollider>().height = perceptionDistance;
+        // GetComponent<CapsuleCollider>().radius = perceptionAngle;
     }
 
     private void Update()
@@ -43,7 +39,7 @@ public class Agent : MonoBehaviour
         {
             path.RemoveAt(0);
 
-            if (path.Count == 0 )
+            if (path.Count == 0)
             {
                 gameObject.SetActive(false);
                 AgentManager.RemoveAgent(gameObject);
@@ -79,13 +75,8 @@ public class Agent : MonoBehaviour
 
     public void ComputePath(Vector3 destination)
     {
-        nma.enabled = true;
-        var nmPath = new NavMeshPath();
-        nma.CalculatePath(destination, nmPath);
-        path = nmPath.corners.Skip(1).ToList();
-        //path = new List<Vector3>() { destination };
-        //nma.SetDestination(destination);
-        nma.enabled = false;
+        path = new List<Vector3>() { destination };
+        // call rrt here
     }
 
     public Vector3 GetVelocity()
@@ -95,103 +86,41 @@ public class Agent : MonoBehaviour
 
     #endregion
 
-    #region Incomplete Functions
-
-    private Vector3 ComputeForce()
+    public Vector3 goalTorque()
     {
-        Vector3 force;
-
-        force = CalculateGoalForce();
-        force += CalculateAgentForce() + CalculateWallForce();
-
-
-        force += Damping(0.1f);
-
-        if (force != Vector3.zero)
-        {
-            return force.normalized * Mathf.Min(force.magnitude, Parameters.maxSpeed);
-        }
-        else
-        {
-            return Vector3.zero;
-        }
+        Vector3 disp = path[0] - transform.position;
+        float angle = Vector3.Angle(disp, transform.forward);
+        if (Mathf.Abs(angle) < 1) return Vector3.zero;
+        return -Vector3.Cross(disp.normalized, transform.forward).normalized;
     }
 
-    private Vector3 Damping(float damp)
+    public void SetThrust()
     {
-        return -damp * rb.velocity;
     }
 
-    private Vector3 CalculateGoalForce()
+    public void AdjectDirection()
     {
-        Vector3 goal = path.FirstOrDefault();
-        return (2 * Parameters.prefSpeed * (goal - transform.position).normalized - rb.velocity) * (mass / Parameters.T);
-    }
+        Vector3 totalTorque = 10000 * goalTorque();
+        Debug.DrawLine(transform.position, transform.position + 5 * goalTorque(), Color.yellow);
 
-    private Vector3 CalculateAgentForce()
-    {
-        Vector3 taForce = Vector3.zero;
-        foreach (var a in perceivedNeighbors)
-        {
-            Vector3 nij = transform.position - a.transform.position;
-            float dij = nij.magnitude;
-            nij.Normalize();
-            float rij = radius + a.GetComponent<Agent>().radius;
-            float g_rd = rij - dij;
-            if (g_rd < 0) g_rd = 0;
-            Vector3 tij = Vector3.ProjectOnPlane(rb.velocity, nij);
-            float del_vjit = tij.magnitude;
-            tij.Normalize();
-            del_vjit += Vector3.Dot(a.GetComponent<Rigidbody>().velocity, -tij);
-            // print(rij - dij + ", " + g_rd);
-
-            taForce += (Parameters.A * Mathf.Exp((rij - dij) / Parameters.B) + Parameters.k * g_rd) * nij - Parameters.Kappa * g_rd * del_vjit * tij;
-        }
-        return taForce;
-    }
-
-    private Vector3 CalculateWallForce()
-    {
-        Vector3 taForce = Vector3.zero;
-        foreach (var a in perceivedWalls)
-        {
-            Vector3 niW = transform.position - a.GetComponent<BoxCollider>().ClosestPoint(transform.position);
-            float diW = niW.magnitude;
-            niW = Vector3.Project(transform.position - a.transform.position, niW);
-            niW.Normalize();
-            float ri = radius;
-            float g_rd = ri - diW;
-            if (g_rd < 0) g_rd = 0;
-            Vector3 vtt = Vector3.ProjectOnPlane(rb.velocity, niW);
-            // print(ri - diW + ", " + g_rd);
-
-            taForce += (Parameters.WALL_A * Mathf.Exp((ri - diW) / Parameters.WALL_B) + Parameters.WALL_k * g_rd) * niW - Parameters.WALL_Kappa * g_rd * vtt;
-        }
-        return taForce;
-    }
-
-    public void ApplyForce()
-    {
-        var force = ComputeForce();
-        force.y = 0;
-
-        rb.AddForce(force * 10, ForceMode.Force);
+        if (totalTorque.magnitude > maxTorque) totalTorque = maxTorque * totalTorque.normalized;
+        rb.AddTorque(totalTorque);
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Agent"))
             perceivedNeighbors.Add(other.gameObject);
-        else if (other.CompareTag("Wall"))
-            perceivedWalls.Add(other.gameObject);
+        else if (other.CompareTag("Obstacle"))
+            perceivedObstacles.Add(other.gameObject);
     }
 
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Agent"))
             perceivedNeighbors.Remove(other.gameObject);
-        else if (other.CompareTag("Wall"))
-            perceivedWalls.Remove(other.gameObject);
+        else if (other.CompareTag("Obstacle"))
+            perceivedObstacles.Remove(other.gameObject);
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -204,5 +133,4 @@ public class Agent : MonoBehaviour
 
     }
 
-    #endregion
 }
--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class AgentManager : MonoBehaviour
 {
@@ -15,9 +13,9 @@ public class AgentManager : MonoBehaviour
     private GameObject agentParent;
     public Vector3 destination;
 
-    public const float UPDATE_RATE = 0.0f;
+    public float UPDATE_RATE = 0.0f;
     private const int PATHFINDING_FRAME_SKIP = 25;
-    
+
     public static AgentManager instance;
 
     #region Unity Functions
@@ -25,25 +23,19 @@ public class AgentManager : MonoBehaviour
     void Awake()
     {
         instance = this;
-        
+
         Random.InitState(0);
 
         agentParent = GameObject.Find("Agents");
         for (int i = 0; i < agentCount; i++)
         {
-            var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
-            NavMeshHit hit;
-            NavMesh.SamplePosition(randPos, out hit, 10, NavMesh.AllAreas);
-            randPos = hit.position + Vector3.up;
+            var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, (Random.value - 0.5f) * agentSpawnRadius, (Random.value - 0.5f) * agentSpawnRadius);
 
             GameObject agent = null;
             agent = Instantiate(agentPrefab, randPos, Quaternion.identity);
             agent.name = "Agent " + i;
             agent.transform.parent = agentParent.transform;
             var agentScript = agent.GetComponent<Agent>();
-            agentScript.radius = 0.3f;// Random.Range(0.2f, 0.6f);
-            agentScript.mass = 1;
-            agentScript.perceptionRadius = 3;
 
             agents.Add(agentScript);
             agentsObjs.Add(agent, agentScript);
@@ -53,45 +45,23 @@ public class AgentManager : MonoBehaviour
 
     }
 
-    
+
     void Update()
     {
         #region Visualization
-        
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (true)
+            var point = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+            var dir = point - Camera.main.transform.position;
+            RaycastHit rcHit;
+            if (Physics.Raycast(point, dir, out rcHit))
             {
-                var point = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
-                var dir = point - Camera.main.transform.position;
-                RaycastHit rcHit;
-                if (Physics.Raycast(point, dir, out rcHit))
-                {
-                    point = rcHit.point;
-                    // SetAgentDestinations(point);
-                    destination = point;
-                }
-            } else
-            {
-                var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
-
-                NavMeshHit hit;
-                NavMesh.SamplePosition(randPos, out hit, 1.0f, NavMesh.AllAreas);
-                print(hit.position);
-                Debug.DrawLine(hit.position, hit.position + Vector3.up * 10, Color.red, 1000000);
-                foreach (var agent in agents)
-                {
-                    //agent.ComputePath(hit.position);
-                }
+                point = rcHit.point;
+                // SetAgentDestinations(point);
+                destination = point;
             }
         }
-
-#if UNITY_EDITOR
-        if (Application.isFocused)
-        {
-            //UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
-        }
-#endif
 
         #endregion
     }
@@ -109,13 +79,15 @@ public class AgentManager : MonoBehaviour
 
             foreach (var agent in agents)
             {
-                agent.ApplyForce();
+                agent.SetThrust();
+                agent.AdjectDirection();
             }
 
             if (UPDATE_RATE == 0)
             {
                 yield return null;
-            } else
+            }
+            else
             {
                 yield return new WaitForSeconds(UPDATE_RATE);
             }
@@ -151,22 +123,15 @@ public class AgentManager : MonoBehaviour
 
     #endregion
 
-    #region Private Functions
-
-    #endregion
-
-    #region Visualization Functions
-
-    #endregion
-
     #region Utility Classes
 
-    private class Tuple<K,V>
+    private class Tuple<K, V>
     {
         public K Item1;
         public V Item2;
 
-        public Tuple(K k, V v) {
+        public Tuple(K k, V v)
+        {
             Item1 = k;
             Item2 = v;
         }
