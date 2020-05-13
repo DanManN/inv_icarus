b631654ca8f8da5398bbe5e47356f71a3295ed1f
Yanshi Luo <kdrobian@hotmail.com>
2020-05-13 16:40:12 -0400

    Update SelectionManager

--- Assets/SelectionManager.cs
+++ Assets/SelectionManager.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material selectedMaterial;
     [SerializeField] private Material defaultMaterial;
+
 
     private Transform _selection;
 
@@ -82,6 +84,7 @@ public class SelectionManager : MonoBehaviour
                             // selected.Add(selection.GetComponent<GameObject>());
                             // A bool for isSelected?
                             agentScript.isSelected = false;
+                            selectionRenderer.material = defaultMaterial;
                         }
                         else
                         {
@@ -89,6 +92,7 @@ public class SelectionManager : MonoBehaviour
                             // selected.Remove(selection.GetComponent<GameObject>());
                             // A bool for isSelected?
                             agentScript.isSelected = true;
+                            selectionRenderer.material = selectedMaterial;
                         }
 
                     }
